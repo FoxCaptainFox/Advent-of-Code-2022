330fc61fbd78d8db1c7dd34760a1fb6b5fe72bbc
Bohdan Khrapak <bodja1999@gmail.com>
2022-12-01 16:48:52 +0200

    Day 1

---
+++ 01.py
@@ -0,0 +1,16 @@
+from Utils import read_data
+
+def get_top_N_elven_supplies_sum(n):
+    data = read_data(1)
+    summed_array = [0]
+    for value in data:
+        if value is None:
+            summed_array.append(0)
+        else:
+            summed_array[-1] += value
+    summed_array.sort()
+    return sum(summed_array[-n:])
+    
+
+print(get_top_N_elven_supplies_sum(1))
+print(get_top_N_elven_supplies_sum(3))
---
+++ Utils.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+DIRECTORY_NAME = "Data"
+
+
+def read_data(file_number, as_int=True, base=10, by_separate_characacters=False):
+    result = []
+    file_name = os.path.join(
+        __location__, f"{DIRECTORY_NAME}/{format(file_number, '02d')}.txt"
+    )
+
+    with open(file_name) as file:
+        while line := file.readline():
+            line = line.rstrip()
+
+            if not as_int:
+                result.append(line)
+                continue
+
+            if by_separate_characacters:
+                number_list = line
+            else:
+                number_list = re.split("\W+", line)
+            
+            for number_as_str in number_list:
+                try:
+                    number = int(number_as_str, base)
+                    result.append(number)
+                except ValueError:
+                    result.append(None)
+    return result
