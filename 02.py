170758408108540d38e534d22ed645ab6883695a
Bohdan Khrapak <bodja1999@gmail.com>
2022-12-02 19:09:30 +0200

    Day 2 part 2

--- 02.py
+++ 02.py
@@ -17,23 +17,35 @@ def calculate_points_1(description):
     return result
 
 def calculate_points_2(description):
-    shape_1 = description.split(" ")[0]
-    if shape_1 == "A":
-        result = 2
-    elif shape_1 == "B":
-        result = 3
-    elif shape_1 == "C":
-        result = 1
-    
-    shape_2 = description.split(" ")[1]
+    shape_1, shape_2 = description.split(" ")
+
     if shape_2 == "Y":
-        result += 3
+        result = 3
+        if shape_1 == "A":
+            result += 1
+        elif shape_1 == "B":
+            result += 2
+        elif shape_1 == "C":
+            result += 3
     elif shape_2 == "Z":
-        result += 6
+        result = 6
+        if shape_1 == "A":
+            result += 2
+        elif shape_1 == "B":
+            result += 3
+        elif shape_1 == "C":
+            result += 1
+    else:
+        if shape_1 == "A":
+            result = 3
+        elif shape_1 == "B":
+            result = 1
+        elif shape_1 == "C":
+            result = 2
     
     return result
 
 
 data = read_data(2, as_int=False)
 print(sum([calculate_points_1(x) for x in data]))
-print([calculate_points_2(x) for x in data])
+print(sum([calculate_points_2(x) for x in data]))
