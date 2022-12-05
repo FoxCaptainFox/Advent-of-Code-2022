06575bcdea2bd3ed78c877de63fd1839dd439665
Bohdan Khrapak <32983948+FoxCaptainFox@users.noreply.github.com>
2022-12-05 23:33:22 +0000

    Day 5

---
+++ 05.py
@@ -0,0 +1,30 @@
+from itertools import zip_longest
+import re
+import copy
+from Utils import read_data_new
+
+data = read_data_new(5)
+data_separator_index = data.index('')
+
+crates_data = [list(x) for x in data[:data_separator_index - 1]]
+crates_data_transposed_filtered_and_reversed = [
+    list(filter(lambda j: j is not None and j != " ", i))[::-1]
+    for i in zip_longest(*crates_data)
+]
+crates_stacks_1 = crates_data_transposed_filtered_and_reversed[1::4]
+crates_stacks_2 = copy.deepcopy(crates_stacks_1)
+
+for instruction_line in data[data_separator_index + 1:]:
+    instruction = [int(number) for number
+        in re.split("move | from | to ", instruction_line) if number != '']
+
+    for _ in range(instruction[0]):
+        crate = crates_stacks_1[instruction[1] - 1].pop()
+        crates_stacks_1[instruction[2] - 1].append(crate)
+
+    crates = crates_stacks_2[instruction[1] - 1][-instruction[0]:]
+    del crates_stacks_2[instruction[1] - 1][-instruction[0]:]
+    crates_stacks_2[instruction[2] - 1] += crates
+
+print(''.join([stack[-1] for stack in crates_stacks_1]))
+print(''.join([stack[-1] for stack in crates_stacks_2]))
