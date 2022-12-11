17569343495532ba8ff4ebc2bac9fe06ae264b81
Bohdan Khrapak <32983948+FoxCaptainFox@users.noreply.github.com>
2022-12-11 13:46:39 +0000

    Added read_file to utils + refactoring

--- utils.py
+++ utils.py
@@ -9,17 +9,20 @@ def _convert_value(value, output_type):
     except ValueError:
         return None
 
-def read_data(file_number, output_type=str, as_separate_characters=False):
-    result = []
+def read_file(file_number):
     file_name = os.path.join(
         __location__, f"{DIRECTORY_NAME}/{format(file_number, '02d')}.txt"
     )
+    with open(file_name) as f:
+        return f.read()
 
-    with open(file_name) as file:
-        while line := file.readline():
-            line = line.rstrip()
-            if as_separate_characters:
-                result.append([_convert_value(ch, output_type) for ch in line])
-            else:
-                result.append(_convert_value(line, output_type))
+
+def read_data(file_number, output_type=str, as_separate_characters=False):
+    result = []
+    
+    for line in read_file(file_number).split("\n"):
+        if as_separate_characters:
+            result.append([_convert_value(ch, output_type) for ch in line])
+        else:
+            result.append(_convert_value(line, output_type))
     return result
